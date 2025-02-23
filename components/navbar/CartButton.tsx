0b69816a7e98debbe834cada04abeca529ca3522
ayush-govindwar <swayush.govindwar@gmail.com>
2025-02-24 00:10:30 +0530

    feat: set up cart component and fixed bug with aschild

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
-function CartButton() {
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import {LuShoppingCart} from 'react-icons/lu'
+async function CartButton() {
+    const numitems = 9;
   return (
-    <div>CartButton</div>
+    <Button asChild
+    variant='outline' size='icon' className="flex justify-center items-center relative">
+        <Link href='/cart'>
+            <LuShoppingCart/>
+            <span className="absolute -top-2 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
+                {numitems}
+            </span>
+        </Link>
+    </Button>
+
   )
 }
 
--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Input } from '../ui/input'
 
 function Navsearch() {
   return (
-    <div>Navsearch</div>
-  )
+    <Input 
+    type='search' 
+    placeholder='search product...' 
+    className='max-w-xs dark:bg-muted'
+    /> //apply style only in dark mode
+    
+)
 }
 
 export default Navsearch
