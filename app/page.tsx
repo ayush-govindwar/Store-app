36b3aff540e76ad1fb0c1bf1a1cd83b40138df33
ayush-govindwar <swayush.govindwar@gmail.com>
2025-02-24 20:21:20 +0530

    feat: set up products grid and  list and product page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
-import FeaturedProducts from "@/components/home/FeaturedProducts";
-import Hero from "@/components/home/Hero";
-import {Button} from "@/components/ui/button";
-
-
+import FeaturedProducts from '@/components/home/FeaturedProducts';
+import Hero from '@/components/home/Hero';
+import LoadingContainer from '@/components/global/LoadingContainer';
+import { Suspense } from 'react';
 function HomePage() {
-  return(
+  return (
     <>
-    <Hero/>
-    <FeaturedProducts/>
-    
+      <Hero />
+      <Suspense fallback={<LoadingContainer />}>
+        <FeaturedProducts />
+      </Suspense>
     </>
-)
-
+  );
 }
-export default HomePage
+export default HomePage;
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,16 @@
-function ProductsPage() {
-    return <div>ProductsPage</div>;
-  
-  }
-  export default ProductsPage
+import ProductsContainer from '@/components/products/ProductsContainer';
+
+async function ProductsPage({ //searchParams contains key-value pairs from the query string of the URL.
+  searchParams, //In Client Components: Use useSearchParams
+}: {
+  searchParams: { layout?: string; search?: string };
+}) {
+  const layout = searchParams.layout || 'grid';
+  const search = searchParams.search || '';
+  return (
+    <>
+      <ProductsContainer layout={layout} search={search} />
+    </>
+  );
+}
+export default ProductsPage;
