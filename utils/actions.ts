80060143cd6f7fa9dbe93981a1257ac3d3fecef6
ayush-govindwar <swayush.govindwar@gmail.com>
2025-02-25 23:03:29 +0530

    feat: added fecthing of products and fixed bugs

--- utils/actions.ts
+++ utils/actions.ts
@@ -17,3 +17,16 @@ export const fetchAllProducts = async ()=>{
         }
     })
 }
+import { redirect } from 'next/navigation';
+
+export const fetchSingleProduct = async (productId: string) => {
+  const product = await db.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+  if (!product) {
+    redirect('/products');
+  }
+  return product;
+};
